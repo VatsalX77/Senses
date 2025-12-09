bfa797f305d8374e06dd3c49291c1123bf1e1aba
Vatsalx77 <vatsalharkhani5911@gmail.com>
2025-12-09 16:14:31 +0530

    Frontend initlization and Folder setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from "@/components/ui/layout/button"
+
+const App = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+    
+  )
+}
+
+export default App
